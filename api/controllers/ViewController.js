7102e74c382557a5e52c206efd702976a26c1021
Lee Longmore <lee.longmore@gmail.com>
2016-07-28 15:19:38 +0100

    Try out a query

--- api/controllers/ViewController.js
+++ api/controllers/ViewController.js
@@ -4,6 +4,10 @@ const Controller = require('trails-controller')
 
 module.exports = class ViewController extends Controller {
   helloWorld(req, res) {
+    var query = "DVLA";
+
+    this.app.orm.Department.query('SELECT * FROM department WHERE name = $1', [query], function(err, results) { console.log('**** ', results[0]) });
+
     res.render('index.html', { asset_path: 'govuk_modules/govuk_template/assets/', name: 'Hello world!' })
   }
 }
