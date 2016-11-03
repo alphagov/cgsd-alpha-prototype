819e7fee23deaa12d4de133d71d42ccf6a0d87ce
Lee Longmore <lee.longmore@gmail.com>
2016-11-03 09:23:49 +0000

    Configure a production environment
    To work with Postgres on localhost

--- config/env/production.js
+++ config/env/production.js
@@ -10,9 +10,34 @@ module.exports = {
     ]
   },
 
+  database : {
+
+    stores: {
+
+      postgres: {
+        adapter: require('waterline-postgresql'),
+        connection: {
+          database: 'pp-alpha',
+          host: 'localhost',
+          user: 'turkeyred',
+          password: '',
+          port: 5432,
+          ssl: false,
+          migrate: 'create'
+        }
+      }
+    },
+
+    models: {
+      defaultStore: 'postgres',
+      migrate: 'create'
+    }
+
+  },
+
   log: {
     logger: new winston.Logger({
-      level: 'info',
+      level: 'debug',
       exitOnError: false,
       transports: [
         new winston.transports.Console({
