1d09b9b1a0f6f5e8a417a544cfa4de8eff46fae5
Lee Longmore <lee.longmore@gmail.com>
2016-12-06 13:51:34 +0000

    Ignore production config when not in production env

--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,54 @@
 'use strict'
 
-module.exports = {}
+const winston = require('winston')
+
+module.exports = {
+
+  database: {
+
+    stores: {
+
+      postgres: {
+        adapter: require('waterline-postgresql'),
+        connection: {
+          database: 'pp_alpha',
+          host: 'localhost',
+          user: 'turkeyred',
+          password: '',
+          port: 5432,
+          ssl: false
+        }
+      }
+    },
+
+    models: {
+      defaultStore: 'postgres',
+      migrate: 'alter'
+    }
+
+  },
+
+  log: {
+    logger: new winston.Logger({
+      level: 'debug',
+      exitOnError: false,
+      transports: [
+        new winston.transports.Console({
+          timestamp: true
+        }),
+        new winston.transports.File({
+          name: 'info-file',
+          level: 'debug',
+          filename: 'trails-info.log',
+          timestamp: true
+        }),
+        new winston.transports.File({
+          name: 'error-file',
+          level: 'error',
+          filename: 'trails-error.log',
+          timestamp: true
+        })
+      ]
+    })
+  }
+}
--- config/env/production.js
+++ config/env/production.js
@@ -2,62 +2,64 @@
 
 const winston = require('winston')
 
-var vcap_services = JSON.parse(process.env.VCAP_SERVICES)
+if (process.env.NODE_ENV == 'production') {
+  var vcap_services = JSON.parse(process.env.VCAP_SERVICES)
 
-module.exports = {
+  module.exports = {
 
-  trailpack: {
-    disabled: [
-      'repl'
-    ]
-  },
-
-  database : {
-
-    stores: {
-
-      postgres: {
-        adapter: require('waterline-postgresql'),
-        connection: {
-          database: vcap_services.postgres[0].credentials.name,
-          host: vcap_services.postgres[0].credentials.host,
-          user: vcap_services.postgres[0].credentials.username,
-          password: vcap_services.postgres[0].credentials.password,
-          port: 5432,
-          ssl: true
-        }
-      }
+    trailpack: {
+      disabled: [
+        'repl'
+      ]
     },
 
-    models: {
-      defaultStore: 'postgres',
-      migrate: 'safe'
+    database : {
+
+      stores: {
+
+        postgres: {
+          adapter: require('waterline-postgresql'),
+          connection: {
+            database: vcap_services.postgres[0].credentials.name,
+            host: vcap_services.postgres[0].credentials.host,
+            user: vcap_services.postgres[0].credentials.username,
+            password: vcap_services.postgres[0].credentials.password,
+            port: 5432,
+            ssl: true
+          }
+        }
+      },
+
+      models: {
+        defaultStore: 'postgres',
+        migrate: 'safe'
+      }
+
+    },
+
+    log: {
+      logger: new winston.Logger({
+        level: 'debug',
+        exitOnError: false,
+        transports: [
+          new winston.transports.Console({
+            timestamp: true
+          }),
+          new winston.transports.File({
+            name: 'info-file',
+            level: 'info',
+            filename: 'trails-info.log',
+            timestamp: true
+          }),
+          new winston.transports.File({
+            name: 'error-file',
+            level: 'error',
+            filename: 'trails-error.log',
+            timestamp: true
+          })
+        ]
+      })
     }
 
-  },
-
-  log: {
-    logger: new winston.Logger({
-      level: 'debug',
-      exitOnError: false,
-      transports: [
-        new winston.transports.Console({
-          timestamp: true
-        }),
-        new winston.transports.File({
-          name: 'info-file',
-          level: 'info',
-          filename: 'trails-info.log',
-          timestamp: true
-        }),
-        new winston.transports.File({
-          name: 'error-file',
-          level: 'error',
-          filename: 'trails-error.log',
-          timestamp: true
-        })
-      ]
-    })
   }
-
 }
