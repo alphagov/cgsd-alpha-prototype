85497c46ccf840bca66b426077ff7f7d4899e16d
Chris Thomas <christopher.thomas@digital.cabinet-office.gov.uk>
2017-01-11 15:20:17 +0000

    New layout applied to show.html template

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -125,3 +125,25 @@ $(document).ready(function() {
   toggleContent.showHideCheckboxToggledContent();
 
 });
+
+
+
+// this code is taken from the gov uk accordion prototype
+
+(function (global) {
+
+  var $ = global.jQuery;
+  var GOVUK = global.GOVUK || {};
+
+$(function () {
+
+  GOVUK.modules.start();
+  var $section = $('.subsection__header');
+  if ($section.length === 1) {
+      $section.trigger('click');
+  }
+
+  GOVUK.modules.start();
+
+});
+})(window);
