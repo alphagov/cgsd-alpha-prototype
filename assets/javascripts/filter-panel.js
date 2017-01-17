8362e614542984f8ba3beee319d710bc2cb40745
Chris Thomas <christopher.thomas@digital.cabinet-office.gov.uk>
2017-01-17 14:00:57 +0000

    Show either number or percentage input box depending on metric selected

--- assets/javascripts/filter-panel.js
+++ assets/javascripts/filter-panel.js
@@ -21,16 +21,80 @@ $(document).ready(function() {
     }
 	});
 
-  // show a shorter input box when a percentage metric is selected
-  $("#select-metric").change(function(){
-    if ($("#select-metric").val() == "online", "phone", "paper", "facetoface", "other") {
-      $(this).closest("fieldset").find(".width-data-input").css("width", "5%").after("<span>% </span>");
-    } 
-  });
-
-	// add another row when clicking 'add filter'
-	$('#add-filter-row').click(function() {
+  // add another row when clicking 'add filter'
+  $('#add-filter-row').click(function() {
     $( '.filter-row:last' ).clone().insertAfter('.filter-row:last');
   });
+
+  // show either a number or percentage box depending on the metric selected
+  // there's definitely a better way to do this but I can't work out how to if/else with arrays of multiple values
+  $("#select-metric").change(function(){
+    if ($("#select-metric").val() == "transactions") {
+      $(".input-number").show();
+      $(".input-percentage").hide();
+    }
+    if ($("#select-metric").val() == "online") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "phone") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "paper") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "facetoface") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "other") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "outcome") {
+      $(".input-number").show();
+      $(".input-percentage").hide();
+    }
+    if ($("#select-metric").val() == "intendedoutcome") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "calls") {
+      $(".input-number").show();
+      $(".input-percentage").hide();
+    }
+    if ($("#select-metric").val() == "getinfo") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "getinfo") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "chaseprogress") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "challengedecision") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "othercall") {
+      $(".input-percentage").show();
+      $(".input-number").hide();
+    }
+    if ($("#select-metric").val() == "numberservices") {
+      $(".input-number").show();
+      $(".input-percentage").hide();
+    }
+    if ($("#select-metric").val() == "numberagencies") {
+      $(".input-number").show();
+      $(".input-percentage").hide();
+    }
+  });
+
+	
  
 });
