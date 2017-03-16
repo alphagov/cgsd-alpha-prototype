d9e20009e955f9ec9611f41dc56e0c8b7fd63976
Chris Thomas <christopher.thomas@digital.cabinet-office.gov.uk>
2017-03-16 16:40:15 +0000

    Show and reveal guidance using toggle button

--- assets/javascripts/filter-panel.js
+++ assets/javascripts/filter-panel.js
@@ -1,3 +1,15 @@
+// showing and hiding the guidance
+
+$(document).ready(function() {
+  $('.metric-guidance').hide();
+  $('.show-guidance button').click(function() {
+    $(this).closest('.filter-cards li').find('.metric-guidance').toggle();
+    $(this).closest('.filter-cards li').find('.metric-name').toggleClass('bold');
+    $(this).find('span').toggle();
+  });
+});
+
+
 // latest filter design
 
 $(document).ready(function() {
